8c8d1eaeef4b76cd689406b836fc0cbc1d9beae1
aunknownwhoami <aunknown.whoami@gmail.com>
2022-12-23 00:00:17 +0530

    new_cfg

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # lvim
+```
+curl -O https://raw.githubusercontent.com/aunknownwhoami/lvim/master/lvim.sh && bash lvim.sh -i
+
+```
--- lvim.sh
+++ lvim.sh
@@ -6,13 +6,17 @@ green='\033[1;32m'
 NC='\033[0m'
 
 #check lvim directory
+install() {
 if [ -d ~/.config/lvim ]; then
     echo -e "${green}lvim directory already exists${NC}"
     echo -e "${red} $0 -r ${NC} to remove lvim directores"
 else
     echo -e "${green}lvim directory does not exist${NC}"
     bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+rm -rf ~/.config/lvim
+git clone https://github.com/aunknownwhoami/lvim.git ~/.config/lvim
 fi
+}
 
 #remove arguments lvim
 rvim() {
@@ -26,4 +30,9 @@ rvim() {
 if [ "$1" = "-r" ]; then
     echo -e "${red}removing lvim${NC}"
     rvim
+fi
+
+if [ "$1" = "-i" ];then
+	echo -e "${green}install lunarvim${NC}"
+	install
 fi
